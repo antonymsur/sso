3838a161ec5ac3cadeb9c055f238be2bc8b70f6c
antonymsur <antony.suresh@gmail.com>
2025-02-13 20:12:20 +0530

    Update views.py missed calling the nonce generation method

--- app2/helpservice/views.py
+++ app2/helpservice/views.py
@@ -14,7 +14,7 @@ def helpservice(request):
 	scope = 'scope=' + settings.AUTH_SCOPE
 	stateOpaqueValue = 'af0ifjsldkj'  #currenly hardcoded. This needs to be generated 
 	state = 'state='+stateOpaqueValue #opaque value used to maintain state between request and call back
-	nonceValue = 'n-0S6_WzA2Mj'       #currenly hardcoded. This needs to be generated 
+	nonceValue = generate_nonce()       #currenly hardcoded. This needs to be generated 
 	nonce = 'nonce='+nonceValue       #associate Client session with an ID Token , this used to mitigate replay attack
 	loginurl = oidpUrl + '&' + appId + '&' +redirectUrl + '&' + scope + '&' + state + '&' +nonce 
     else:
